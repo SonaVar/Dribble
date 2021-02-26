2e4b7f9781a8b3e71018c9bee1847a7d8429f8a5
Sona Varghese <sonasusan.v@gmail.com>
2021-02-25 21:41:09 -0500

    added stretch feature of animated URL property and shot title on hover

--- src/components/ShotsGridItem.js
+++ src/components/ShotsGridItem.js
@@ -1,32 +1,78 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import "./ShotsGridItem.scss"
+import "./ShotsGridItem.scss";
+import useWindowSize from "../hooks/useWindowSize";
 
-import Image from 'react-bootstrap/Image';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ModeCommentIcon from '@material-ui/icons/ModeComment';
-
+import Image from "react-bootstrap/Image";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ModeCommentIcon from "@material-ui/icons/ModeComment";
 
 export default function ShotsGridItem(props) {
+  const [image, setImage] = useState(props.stillUrl);
+  const [title, setTitle] = useState("");
+  const [isShown, setIsShown] = useState(false);
+
+  const size = useWindowSize();
+  console.log(size)
+  
+  const mouseOverEvent = (bool) => {
+    if (bool) {
+      setTitle(props.title);
+      setIsShown(true);
+      if (props.videoUrl) {
+        setImage(props.videoUrl);
+      }
+    } else {
+      setIsShown(false);
+      setImage(props.stillUrl);
+    }
+  };
 
   return (
-    <div className='user-shot__item' data-testid="shots">
-      <Image className='user-shot__image' src={props.stillUrl}></Image>
-      <div className='user-shot__name'>
-        <div className='user-shot__box'>
-          <Image className='user-shot__avatar' src={props.author.avatarUrl}/>
-          <h5>{props.author.name}</h5>
-          <div className='user-shot__label'>{props.author.isPro ? 'PRO' : 'TEAM'}</div>
+    <div className="user-shot__item" data-testid="shots">
+      <picture
+        onMouseOver={() => mouseOverEvent(true)}
+        onMouseLeave={() => mouseOverEvent(false)}
+        title={title}
+      >
+        <source type="video/mp4" srcSet={image} />
+        <source type={props.mimeType} srcSet={image} />
+        <img class="user-shot__image" src={image} alt="user-shot" />
+      </picture>
+      {isShown && (
+        <div className="user-shot__overlay">
+          <div className="user-shot__overlay-content">
+            <div className="user-shot__overlay-text">{props.title}</div>
+          </div>
         </div>
-        <section className='user-shot__container'>
-          <div className='user-shot__section'>
-            <ModeCommentIcon className='user-shot__comment' color="disabled" fontSize="inherit"></ModeCommentIcon>
+      )}
+      <div className="user-shot__name">
+        <div className="user-shot__box">
+          <a href={props.author.profileUrl}>
+          < Image className="user-shot__avatar" src={props.author.avatarUrl} onClick/>
+          </a>
+          <h5>{props.author.name}</h5>
+          <div className="user-shot__label">
+            {props.author.isPro ? "PRO" : "TEAM"}
+          </div>
+        </div>
+        <section className="user-shot__container">
+          <div className="user-shot__section">
+            <ModeCommentIcon
+              className="user-shot__comment"
+              color="disabled"
+              fontSize="inherit"
+            ></ModeCommentIcon>
             {props.commentsCount}
-            <FavoriteIcon className='user-shot__like' color="disabled" fontSize="inherit"></FavoriteIcon>
+            <FavoriteIcon
+              className="user-shot__like"
+              color="disabled"
+              fontSize="inherit"
+            ></FavoriteIcon>
             {props.likesCount}
           </div>
         </section>
       </div>
     </div>
   );
-};
+}
